98d9cb9332b9bb37dce7c2ef774c62025ffe29f7
removedporn <86824510+removedporn@users.noreply.github.com>
2022-01-04 00:29:29 +0800

    Delete test directory

--- test/minitest_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-require 'minitest/spec'
-require 'valid_attribute/minitest'
-
-describe 'Minitest::Spec' do
-  it '.have_valid' do
-    have_valid(:name).must_be_instance_of(ValidAttribute::Matcher)
-  end
-end
